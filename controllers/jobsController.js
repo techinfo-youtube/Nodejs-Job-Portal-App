3a1a8d1266ec7007dfc4cec219fd37c0c2dee3f2
Rasheed khan <rktechnicalplus@gmail.com>
2023-04-06 15:43:54 +0530

    job crud operations
    complete code till job crud

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -19,3 +19,46 @@ export const getAllJobsController = async (req, res, next) => {
     jobs,
   });
 };
+
+// ======= UPDATE JOBS ===========
+export const updateJobController = async (req, res, next) => {
+  const { id } = req.params;
+  const { company, position } = req.body;
+  //validation
+  if (!company || !position) {
+    next("Please Provide All Fields");
+  }
+  //find job
+  const job = await jobsModel.findOne({ _id: id });
+  //validation
+  if (!job) {
+    next(`no jobs found with this id ${id}`);
+  }
+  if (!req.user.userId === job.createdBy.toString()) {
+    next("Your Not Authorized to update this job");
+    return;
+  }
+  const updateJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  //res
+  res.status(200).json({ updateJob });
+};
+
+// ======= DELETE JOBS ===========
+export const deleteJobController = async (req, res, next) => {
+  const { id } = req.params;
+  //find job
+  const job = await jobsModel.findOne({ _id: id });
+  //validation
+  if (!job) {
+    next(`No Job Found With This ID ${id}`);
+  }
+  if (!req.user.userId === job.createdBy.toString()) {
+    next("Your Not Authorize to delete this job");
+    return;
+  }
+  await job.deleteOne();
+  res.status(200).json({ message: "Success, Job Deleted!" });
+};
--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {
   createJobController,
+  deleteJobController,
   getAllJobsController,
+  updateJobController,
 } from "../controllers/jobsController.js";
 import userAuth from "../middelwares/authMiddleware.js";
 
@@ -13,5 +15,11 @@ router.post("/create-job", userAuth, createJobController);
 
 //GET JOBS || GET
 router.get("/get-job", userAuth, getAllJobsController);
+
+//UPDATE JOBS ||  PATCH
+router.patch("/update-job/:id", userAuth, updateJobController);
+
+//DELETE JOBS || DELETE
+router.delete("/delete-job/:id", userAuth, deleteJobController);
 
 export default router;
