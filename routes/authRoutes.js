82bfdf006eb992f8eeac001580e5f99d9a1c895c
Rasheed khan <rktechnicalplus@gmail.com>
2023-04-11 15:23:53 +0530

    app security
    complete code till application security

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,15 @@ import {
   loginController,
   registerController,
 } from "../controllers/authController.js";
+import rateLimit from "express-rate-limit";
+
+//ip limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 //router object
 const router = express.Router();
@@ -10,10 +19,10 @@ const router = express.Router();
 //routes
 
 // REGISTER || POPST
-router.post("/register", registerController);
+router.post("/register", limiter, registerController);
 
 // LOGIN || POST
-router.post("/login", loginController);
+router.post("/login", limiter, loginController);
 
 //export
 export default router;
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ import dotenv from "dotenv";
 import colors from "colors";
 import cors from "cors";
 import morgan from "morgan";
+//securty packges
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
 // files imports
 import connectDB from "./config/db.js";
 // routes import
@@ -24,6 +28,9 @@ connectDB();
 const app = express();
 
 //middelwares
+app.use(helmet(``));
+app.use(xss());
+app.use(mongoSanitize());
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
