a98927d117210e41a1748c7b472f85851a8f6c02
Techinfo YT <49879287+techinfo-youtube@users.noreply.github.com>
2023-04-15 01:48:00 +0530

    updated

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:8080",
+//         url: "http://localhost:8080",
+            url: "https://nodejs-job-portal-app.onrender.com"
       },
     ],
   },
